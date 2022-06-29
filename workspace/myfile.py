9b4bef6c2fc870f089fbdcb378965bbc80ac2506
teshome-p <108298509+teshome-p@users.noreply.github.com>
2022-06-29 02:47:57 -0400

    updated youtube API file day 2

--- workspace/myfile.py
+++ workspace/myfile.py
@@ -1,5 +1,6 @@
-the_api_key = "AIzaSyBmiAPKcX6M0KrzQkQ8ak0RtzV2Uu87rck"
 from pyyoutube import Api
+import pprint
+the_api_key = "AIzaSyBmiAPKcX6M0KrzQkQ8ak0RtzV2Uu87rck"
 api = Api(api_key=the_api_key)
 channel_by_id = api.get_channel_info(channel_id="UC_x5XG1OV2P6uZZ5FSM9Ttw")
-print(channel_by_id.items)
+print(channel_by_id.items[0].to_dict()["snippet"]["description"])
